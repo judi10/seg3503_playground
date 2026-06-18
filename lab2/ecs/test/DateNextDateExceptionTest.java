8bf6f04eeff03cb6b9f4a52ec17405f20e794308
judi10 <judicaeltkm@gmail.com>
2026-06-17 22:59:40 -0400

    lab02: equivalence class tests + README + assets

--- lab2/ecs/test/DateNextDateExceptionTest.java
+++ lab2/ecs/test/DateNextDateExceptionTest.java
@@ -1,5 +1,58 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class DateNextDateExceptionTest
 {
+
+    // 1500 02 31 IllegalArgumentException
+    
+    @Test
+    void test_february_31_day(){
+        assertThrows (
+            IllegalArgumentException.class,
+            () -> new Date(1500,02,31)
+        );
+        
+    }
+    @Test
+    void test_february_29_day(){
+        assertThrows (
+            IllegalArgumentException.class,
+            () -> new Date(1500,02,29)
+        );
+        
+    }
+
+    @Test
+    void negative_year(){
+        assertThrows (
+            IllegalArgumentException.class,
+            () -> new Date(-1,10,20)
+        );
+        
+    }
+
+    @Test
+    void more_than_twelveMonth(){
+        assertThrows (
+            IllegalArgumentException.class,
+            () -> new Date(1458,15,12)
+        );
+        
+    }
+
+    @Test
+    void negative_day(){
+        assertThrows (
+            IllegalArgumentException.class,
+            () -> new Date(1975,6,-50)
+        );
+        
+    }
+
+
 
 
 }
--- lab2/ecs/test/DateNextDateOkTest.java
+++ lab2/ecs/test/DateNextDateOkTest.java
@@ -1,5 +1,57 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 public class DateNextDateOkTest
 {
+    
+    @Test
+    void test_next_date_normal_day(){
+        Date d = new Date(1700, 6, 20);
+        Date next = d.nextDate();
+        assertEquals(new Date(1700 , 6, 21), next);
+    }
+
+    @Test
+    void test_next_date_end_month_31_days(){
+        Date d = new Date(2026, 1, 31);
+        Date next = d.nextDate();
+        assertEquals(new Date( 2026, 2, 1), next);
+    }
+
+    @Test
+    void test_next_date_end_month_30_days(){
+        Date d = new Date(1, 4, 30);
+        Date next = d.nextDate();
+        assertEquals(new Date( 1, 5, 1), next);
+    }
+
+    @Test
+    void test_next_date_end_feb_bissextile(){
+        Date d = new Date(2000, 2, 29);
+        Date next = d.nextDate();
+        assertEquals(new Date( 2000, 3, 1), next);
+    }
+
+    @Test
+    void test_next_date_end_feb_no_bissextile(){
+        Date d = new Date(4001, 2, 28);
+        Date next = d.nextDate();
+        assertEquals(new Date( 4001, 3, 1), next);
+    }
+
+    @Test
+    void test_next_date_end_feb_no_bissextile_siecle(){
+        Date d = new Date(1500 , 2, 28);
+        Date next = d.nextDate();
+        assertEquals(new Date( 1500, 3, 1), next);
+    }
+
+    @Test
+    void test_next_date_end_year(){
+        Date d = new Date(5, 12, 31);
+        Date next = d.nextDate();
+        assertEquals(new Date( 6, 1, 1), next);
+    }
+
 
 
 }
