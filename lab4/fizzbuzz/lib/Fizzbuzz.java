1343e3aa453d17eda1300bdf47b713d4956bc6fc
judi10 <judicaeltkm@gmail.com>
2026-06-26 18:45:41 -0400

    VERT - test15 FizzBuzz pass [PASS]

--- lab4/fizzbuzz/lib/Fizzbuzz.java
+++ lab4/fizzbuzz/lib/Fizzbuzz.java
@@ -11,6 +11,7 @@ public class Fizzbuzz {
         if (n == 3) return "Fizz";
         if (n == 6) return "Fizz";
         if (n == 9) return "Fizz";
+        if (n== 15) return "FizzBuzz";
         return null;
     }
     
